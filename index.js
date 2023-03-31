d05c094b09e95f551acc0f8d5817a8123aabe58a
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-03-31 17:52:48 -0400

    arithmetic_lab_complete

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
+let num1 = 31
+let num2 = 2
+let multiply = (num1,num2=62)
+    31*2
+
+    
+
+Math.random(0,1)
+let random = Math.floor(Math.random(0,1) * 10) + 1;
+console.log(Math.random);
+
+let num3 = 60
+let num4 = 8
+let mod  = (num3,num4=4)
+            num3 % num4
+
+Math.max(5,10,15,20)
+let max =  20
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
 
 // Write your code here
